0a2a521d25667c1c1eceb2e4dda01318a7a100e4
r3gis.3R@gmail.com <r3gis.3R@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2010-10-16 11:58:09 +0000

    Platform armv4t support
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk@280 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- pjsip_android/build_release.sh
+++ pjsip_android/build_release.sh
@@ -1,39 +1,58 @@
-#!/bin/sh
+#!/bin/bash
 
 usage() {
-	echo -n "Usage: $0 [-h] [-d]\n-h : show this help\n-d : turn debugging on\n" 
-} 
+	echo -n "Usage: $0 [-h] [-d] [-t target abi] [-v version]\n-h : show this help\n-d : turn debugging on\n-t specify target eabis\n" 
+}
+
 
 MAKE_FLAGS=""
 VERSION="undefined"
 ACTION=""
+TARGETS="armeabi armeabi-v7a"
 
-while getopts dhcv: arg
+while getopts dhcv:t: arg
 do	case "$arg" in
 	d)	MAKE_FLAGS="NDK_LOG=true V=1";;
 	h)	usage
 		exit 0;;
 	c)	ACTION="clean";;
-	v)  VERSION="$OPTARG";;
+	v)	VERSION="$OPTARG";;
+	t)	TARGETS="$OPTARG";;
 	[?])	usage	
 		exit 1;;
 	esac
 done
 
-for target_path in build/platforms/*; do
-	TARGET_PLATEFORM=`basename $target_path`
-	OUT_BUILD_PATH="apps/pjsip/project/libs/armeabi/$TARGET_PLATEFORM"
-	rm $OUT_BUILD_PATH/*
-	
+$old_IFS = $IFS
+IFS=" "
+
+for abi in $TARGETS; do
+	#Use the supported platform
+	platform="android-4"
+	if [[ $abi == "armv4t" ]]; then
+		platform="android-3"
+	elif [[ $abi == "x86" ]]; then
+		platform="android-8"
+	elif [[ $abi == "armeabi-v7a" ]]; then
+		platform="android-8"
+	fi
 	#Build the stack
-	echo "Make for $TARGET_PLATEFORM"
-	make APP=pjsip APP_PLATFORM=$TARGET_PLATEFORM $MAKE_FLAGS $ACTION
-	
+	echo "Make for $abi using $platform"
+	make APP=pjsip APP_ABI=$abi APP_PLATFORM=$platform $MAKE_FLAGS $ACTION
+
+
 	#create bundle and md5 for publish
-	OUT_NAME=${TARGET_PLATEFORM}_libpjsipjni-${VERSION}.so.gz
+	OUT_BUILD_PATH="apps/pjsip/project/libs/$abi"
+	OUT_NAME=android-${abi}_libpjsipjni-${VERSION}.so.gz
 	OUT_TGZ=$OUT_BUILD_PATH/$OUT_NAME
-	echo "Create bundle $OUT_NAME"
-	gzip -c $OUT_BUILD_PATH/libpjsipjni.so > $OUT_TGZ
-	md5sum $OUT_TGZ > $OUT_BUILD_PATH/$OUT_NAME.md5sum
+
+	if [[ $ACTION == "clean" ]]; then
+		rm $OUT_TGZ*
+	else
+		echo "Create bundle $OUT_NAME"
+		gzip -c $OUT_BUILD_PATH/libpjsipjni.so > $OUT_TGZ
+		md5sum $OUT_TGZ > $OUT_BUILD_PATH/$OUT_NAME.md5sum
+	fi
 done
 
+IFS=$old_IFS
