dc1b00d46c41443abf6972400427ad38a7c7779b
r3gis.3R@gmail.com <r3gis.3R@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2010-12-12 22:24:13 +0000

    Update android module license
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk@435 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- pjsip_android/apps/pjsip/project/jni/include/android_dev.h
+++ pjsip_android/apps/pjsip/project/jni/include/android_dev.h
@@ -1,8 +1,18 @@
-/*
- * android_dev.h
+/**
+ * Copyright (C) 2010 Regis Montoya (aka r3gis - www.r3gis.fr)
+ * This file is part of pjsip_android.
  *
- *  Created on: 11 déc. 2010
- *      Author: r3gis3r
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef ANDROID_DEV_H_
