9ef0fb54c483f93e46e3573a1957dd1ffa90df72
r3gis.3R@gmail.com <r3gis.3R@gmail.com>
2010-12-12 22:24:13 +0000

    Update android module license
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk@435 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- pjsip_android/apps/pjsip/project/jni/src/android_jni_dev.cpp
+++ pjsip_android/apps/pjsip/project/jni/src/android_jni_dev.cpp
@@ -1,30 +1,22 @@
-/* $Id: android_dev.cpp $ */
 /**
- * This file is part of CSipSimple.
+ * Copyright (C) 2010 Regis Montoya (aka r3gis - www.r3gis.fr)
+ * This file is part of pjsip_android.
  *
- *  CSipSimple is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  CSipSimple is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  You should have received a copy of the GNU General Public License
- *  along with CSipSimple.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * Since deeply inspired from portaudio dev port:
- * Copyright (C) 2009-2010 r3gis (http://www.r3gis.fr)
- * Copyright (C) 2008-2009 Teluu Inc. (http://www.teluu.com)
- * Copyright (C) 2003-2008 Benny Prijono <benny@prijono.org>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 //This file is a port for android devices
 // It's deeply inspired from port audio
-
 
 #include "android_dev.h"
 
