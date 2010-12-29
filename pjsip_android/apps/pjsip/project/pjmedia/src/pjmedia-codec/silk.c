5c32a6f3e00bbbeefb4573edf5799abc7d93ea12
r3gis.3R@gmail.com <r3gis.3R@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2010-12-29 21:51:22 +0000

    Finalize silk commit
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk@545 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- pjsip_android/apps/pjsip/project/pjmedia/src/pjmedia-codec/silk.c
+++ pjsip_android/apps/pjsip/project/pjmedia/src/pjmedia-codec/silk.c
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2010 Regis Montoya (aka r3gis - www.r3gis.fr)
+ * This file is part of pjsip_android.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <pjmedia/codec.h>
 #include <pjmedia/endpoint.h>
@@ -9,8 +25,6 @@
 #include <pj/assert.h>
 #include <pj/log.h>
 
-#warning Remove that
-#define PJMEDIA_HAS_SILK_CODEC 1
 
 #if defined(PJMEDIA_HAS_SILK_CODEC) && (PJMEDIA_HAS_SILK_CODEC!=0)
 #include "../../third_party/silk/interface/SKP_Silk_SDK_API.h"
