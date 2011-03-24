80397b4c91f1c2c20fc8ae46e374e9fe294b00e7
r3gis.3r@gmail.com <r3gis.3r@gmail.com@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2011-03-24 10:13:51 +0000

    * Update pjsip trunk for time before reg refresh  * Remove pjsip-jni perl and replace by python scripts to produce final lib useable for android
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk@745 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- pjsip_android/swig_tools/add_swig_rename.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-$/ = undef;
-$_ = <>;
-
-s/PJ_DECL.*\s+((pjsua_([A-Za-z1-9_\-]+))\s*\([^)]+\));/%rename($3) $2;\n%javamethodmodifiers $1 "public synchronized";\n$&/g;
-print;
--- pjsip_android/swig_tools/strip_unnecessary_stuff.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-$/ = undef;
-$_ = <>;
-
-# Everything before PJ_BEGIN_DECL
-s/.*PJ_BEGIN_DECL//msg;
-# Everything after PG_END_DECL
-s/PJ_END_DECL.*//msg;
-print;
