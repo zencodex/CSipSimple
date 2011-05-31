bd04cc75a590f08fc65de2486cf86c5b061b4bf0
r3gis.3r@gmail.com <r3gis.3r@gmail.com>
2011-05-31 11:16:23 +0000

    Related to issue 935.
    Thx to Alex.
    The default of use_zrtp should be set as default setting for account.
    
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk@896 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- pjsip_android/apps/pjsip/project/pjsip/src/pjsua-lib/pjsua_acc.c
+++ pjsip_android/apps/pjsip/project/pjsip/src/pjsua-lib/pjsua_acc.c
@@ -846,6 +846,9 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
     acc->cfg.srtp_secure_signaling = cfg->srtp_secure_signaling;
     acc->cfg.srtp_optional_dup_offer = cfg->srtp_optional_dup_offer;    
 #endif
+#if defined(PJMEDIA_HAS_ZRTP) && (PJMEDIA_HAS_ZRTP != 0)
+    acc->cfg.use_zrtp = PJSUA_DEFAULT_USE_ZRTP;
+#endif
 
 #if defined(PJMEDIA_STREAM_ENABLE_KA) && (PJMEDIA_STREAM_ENABLE_KA != 0)
     acc->cfg.use_stream_ka = cfg->use_stream_ka;
