3eae8a1d789ee3d1bb6c262e0b9aea74fdb8eedb
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2012-07-22 11:56:38 +0000

    Add ABS support for checked state in action bar buttons.
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk@1719 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- ActionBarSherlock/src/com/actionbarsherlock/internal/view/menu/ActionMenuItemView.java
+++ ActionBarSherlock/src/com/actionbarsherlock/internal/view/menu/ActionMenuItemView.java
@@ -145,6 +145,7 @@ public class ActionMenuItemView extends LinearLayout
 
         setVisibility(itemData.isVisible() ? View.VISIBLE : View.GONE);
         setEnabled(itemData.isEnabled());
+        setChecked(itemData.isChecked());
     }
 
     @Override
@@ -172,8 +173,25 @@ public class ActionMenuItemView extends LinearLayout
         // TODO Support checkable action items
     }
 
+    private static final int[] CHECKED_STATE_SET = {
+        android.R.attr.state_checked
+    };
+    private boolean checkedState = false;
     public void setChecked(boolean checked) {
-        // TODO Support checkable action items
+        if(checkedState != checked) {
+            checkedState = checked;
+            refreshDrawableState();
+        }
+        
+    }
+    
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (checkedState) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        }
+        return drawableState;
     }
 
     public void setExpandedFormat(boolean expandedFormat) {
