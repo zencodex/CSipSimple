42b9d6857d0a26367a9b4fb94052df170f8c7b32
r3gis3r <r3gis3r@9f815046-5998-e9c0-b7e2-ac03a23edfa4>
2013-02-07 00:17:37 +0000

    Delete obsolete folder not used anymore
    git-svn-id: http://csipsimple.googlecode.com/svn/trunk@2121 9f815046-5998-e9c0-b7e2-ac03a23edfa4

--- pjsip_android/swig_tools/find_missing_types.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ls src/org/pjsip/pjsua/SWIGTYPE_p_* | grep -v '^SWIGTYPE_p_f_' | sed 's/SWIGTYPE_p_//;s/\.java//; s/src\/org\/pjsip\/pjsua\///' | grep -v -e '^f_' -e '^void$' -e '^unsigned_int$' -e '^int$'
--- pjsip_android/swig_tools/print_funcs.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-$/ = undef;
-$_ = <>;
-
-while(/(PJ_DECL[^;]*;)/g) {
-	print "$1\n";
-}
